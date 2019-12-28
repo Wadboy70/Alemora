a73731c7ddf129d2fdbeb43c6153d9bb0529af3f
Wadboy70 <s.oluwaleimu@gmail.com>
2019-12-28 13:08:36 -0500

    most of carousel css has been written

---
+++ js/homePage.js
@@ -0,0 +1,8 @@
+var carousel = document.querySelector(".carousel");
+var carouselItem = document.querySelectorAll(".carousel-item");
+
+carouselItem.forEach((item, count, slides)=>{
+    item.addEventListener("click", ()=>{
+        
+   });
+});
